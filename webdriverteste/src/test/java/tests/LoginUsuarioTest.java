9344c3c37db0d89284c04f8097a518be8c83a752
Sidney304 <79165736+Sidney304@users.noreply.github.com>
2021-02-16 18:12:56 -0300

    Inclusão de projeto Teste
    Inclusão de projeto com uma asserção em teste, utilizando o IntelliJ com Maven

---
+++ webdriverteste/src/test/java/tests/LoginUsuarioTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LoginUsuarioTest {
+
+    @Test
+    public void Login(){
+        assertEquals(1,1);
+
+    }
+}
